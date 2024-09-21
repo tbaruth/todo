ea7560375a1fd80eae2b219ebe1aa483cff6ebda
InsaneWallaby <tristan.baruth@gmail.com>
2024-09-21 15:15:48 -0500

    Git repo initialization.

---
+++ todo-core/src/main/java/com/tbaruth/todocore/TodoCoreApplication.java
@@ -0,0 +1,13 @@
+package com.tbaruth.todocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodoCoreApplication.class, args);
+  }
+
+}
---
+++ todo-core/src/test/java/com/tbaruth/todocore/TodoCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbaruth.todocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
